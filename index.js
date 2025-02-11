5a22ebac62a9e1cff6674adfffdffd343a9c1b81
Aishashaik16 <aishashaik090@gmail.com>
2025-02-11 21:23:13 +0530

    json

---
+++ index.js
@@ -0,0 +1,54 @@
+async function saveData() {
+    let options = {
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": JSON.stringify({    // the json can not take as string it takes only objects
+            "task": task_n.value // it is used to get input entered values
+        })
+    }
+    let response = await fetch("http://localhost:3000/time-table", options);
+
+    if (response.ok) {
+        task_n.value = '';
+        getData();
+    }
+}
+
+async function getData() {
+    let response = await fetch("http://localhost:3000/time-table");
+    let data = await response.json();
+    displayData(data);
+}
+function displayData(data) {
+    let container = document.getElementById("container");
+    container.innerText = ``;
+    data.forEach(obj => {
+        let item = document.createElement("div");
+        item.innerHTML = `
+            <p>${obj.task}</p>
+            <button onclick='deleteData("${obj.id}")'><i class="bi bi-trash3"></i></button>
+        `;
+        container.appendChild(item);
+    });
+}
+
+async function deleteData(id) {
+    let options = {
+        "method": "DELETE"
+    }
+    let response = await fetch(`http://localhost:3000/time-table/${id}`, options);
+    if (response.ok) {
+        console.log("Deleted");
+        getData();
+    }
+}
+
+
+async function deleteAllData() {
+    let response = await fetch("http://localhost:3000/time-table", { method: "GET" });
+    let data = await response.json();
+    data.forEach(obj => deleteData(obj.id));
+}
+getData();
